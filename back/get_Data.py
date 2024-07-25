13d1dfe5780bc0a70cadfdab165324e2e4d4e946
Mario <mariocastillosan@gmail.com>
2024-07-25 18:59:28 -0400

    fix: /

--- back/get_Data.py
+++ back/get_Data.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 def get_Data()-> pd.DataFrame:
-    df = pd.read_excel('data\CorpusEtiquetado_Sampled.xlsx')
+    df = pd.read_excel('data/CorpusEtiquetado_Sampled.xlsx')
     return df
