25926eba34673da880ee1c3c7d12c312fe445817
Mario <mariocastillosan@gmail.com>
2024-07-25 21:47:34 -0400

    update: load model

--- RandomForestModel/load_Model.py
+++ RandomForestModel/load_Model.py
@@ -1,11 +1,4 @@
 import joblib
-
 def load_model(filename):
-    try:
-        loaded_objects = joblib.load(filename)
-        if isinstance(loaded_objects, tuple) and len(loaded_objects) == 3:
-            return loaded_objects
-        else:
-            raise ValueError("Loaded object is not a tuple with 3 elements.")
-    except Exception as e:
-        return None, None, None
+    model, vectorizer, scaler = joblib.load(filename)
+    return model, vectorizer, scaler
