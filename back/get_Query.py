47516dc76b5a9460b4274aea1ed5635f29b6a1e8
Mario <mariocastillosan@gmail.com>
2024-07-26 01:41:53 -0400

    test

--- back/get_Query.py
+++ back/get_Query.py
@@ -13,31 +13,28 @@ logging.basicConfig(level=logging.DEBUG)
 loaded_model, loaded_vectorizer, loaded_scaler = load_model('RandomForestModel/multi_target_forest.pkl')
 
 
-def get_QueryResponse(text: str, model=loaded_model, vectorizer=loaded_vectorizer, scaler=loaded_scaler) -> np.ndarray:
-    try:
-        # Procesar el texto
-        text_processed = text.lower()
-        text_processed = re.sub(r'[^\w\s]', '', text_processed)
+def get_QueryResponse(text: str, model=loaded_model, vectorizer=loaded_vectorizer, scaler=loaded_scaler):
+    # Procesar el texto
+    text_processed = text.lower()
+    text_processed = re.sub(r'[^\w\s]', '', text_processed)
 
-        # Vectorizar el texto
-        text_vectorized = vectorizer.transform([text_processed])
+    # Vectorizar el texto
+    text_vectorized = vectorizer.transform([text_processed])
 
-        # Crear un vector nulo para 'votaciones_Nombre'
-        total_features = scaler.n_features_in_
-        text_features = text_vectorized.shape[1]
-        name_vector_size = total_features - text_features
-        name_vector = np.zeros((1, name_vector_size))
+    # Crear un vector nulo para 'votaciones_Nombre'
+    total_features = scaler.n_features_in_
+    text_features = text_vectorized.shape[1]
+    name_vector_size = total_features - text_features
+    name_vector = np.zeros((1, name_vector_size))
 
-        # Combinar características de texto con el vector nulo
-        Y_test_combined = np.hstack((name_vector, text_vectorized.toarray()))
+    # Combinar características de texto con el vector nulo
+    Y_test_combined = np.hstack((name_vector, text_vectorized.toarray()))
 
-        # Escalar las características
-        Y_test_scaled = scaler.transform(Y_test_combined)
+    # Escalar las características
+    Y_test_scaled = scaler.transform(Y_test_combined)
 
-        # Predecir las etiquetas
-        y_pred = model.predict(Y_test_scaled)
+    # Predecir las etiquetas
+    y_pred = model.predict(Y_test_scaled)
 
-        return y_pred
-    except Exception as e:
-        logging.error(f"Error during prediction: {e}")
-        return np.array([])  
+    return y_pred
+
