e9c108dea12bafb86d0a77ddb8351b748b75b1b4
lucho <lfjoc123@gmail.com>
2024-08-04 19:37:29 -0400

    Explicacion de nuestro desarrollo
    De realiza las debidas explicaciones del paso a paso y que contiene nuestra base de datos.

--- back/get_Graph.py
+++ back/get_Graph.py
@@ -5,17 +5,17 @@ import plotly.express as px
 def get_CombinationGraph(data):
     if not data.empty:
         # Procesar combinaciones de etiquetas
-        etiqueta_columns = data.columns 
+        etiqueta_columns = data.columns
         etiqueta_names = [col.replace('Etiqueta ', '') for col in etiqueta_columns]
         data_comb = data[etiqueta_columns].apply(lambda x: ','.join([etiqueta_names[i] for i in range(len(x)) if x.iloc[i] == 1]), axis=1)
         combinaciones = data_comb.value_counts().sort_values(ascending=False)
-        
+
         # Crear gráfico de barras para combinaciones de etiquetas
         fig = px.bar(combinaciones, x=combinaciones.index, y=combinaciones.values, title='Combinaciones de Etiquetas')
         fig.update_xaxes(title='Combinaciones', tickangle=45)
         fig.update_yaxes(title='Cantidad')
         fig.update_layout(xaxis={'categoryorder':'total descending'}, xaxis_tickangle=-45)
-        
+
         # Configurar zoom inicial y scroll horizontal
         fig.update_layout(
             xaxis=dict(
@@ -26,7 +26,7 @@ def get_CombinationGraph(data):
             height=600,
             margin=dict(l=40, r=40, t=40, b=40),
         )
-        
+
         st.plotly_chart(fig)
     else:
         st.warning("No se pudo obtener el dataframe.")
@@ -35,12 +35,12 @@ def get_CountGraph(data):
     # Crear un gráfico que muestre cuántos textos hay con n etiquetas
     num_etiquetas = data.sum(axis=1)
     etiquetas_counts = num_etiquetas.value_counts().sort_index()
-    
+
     # Crear gráfico de barras
     fig = px.bar(x=etiquetas_counts.index, y=etiquetas_counts.values, title='Cantidad de Textos por Número de Etiquetas')
     fig.update_xaxes(title='Número de Etiquetas')
     fig.update_yaxes(title='Cantidad de Textos')
-    
+
     st.plotly_chart(fig)
 
 def get_LabelCountGraph(labels, counts):
@@ -48,10 +48,10 @@ def get_LabelCountGraph(labels, counts):
     sorted_indices = sorted(range(len(counts)), key=lambda i: counts[i], reverse=True)
     etiquetas_sorted = [labels[i] for i in sorted_indices]
     cantidades_sorted = [counts[i] for i in sorted_indices]
-    
+
     # Crear gráfico de barras con Streamlit y Plotly
     fig = px.bar(x=etiquetas_sorted, y=cantidades_sorted, title='Conteo de Etiquetas')
     fig.update_xaxes(title='Etiquetas', categoryorder='total descending')
     fig.update_yaxes(title='Cantidad')
-    
+
     st.plotly_chart(fig)
--- views/consulta.py
+++ views/consulta.py
@@ -15,15 +15,16 @@ def mostrar_dataframe_responsivo(df):
 
 def show_consulta(data,actualizar_dataframe):
     st.title("Consulta de Etiquetas")
-    
+
     if st.button("Actualizar datos del dataframe"):
         data = actualizar_dataframe()
     else:
         data = actualizar_dataframe()
 
-    if not data.empty:      
+    if not data.empty:
         # Campo de entrada de número de filas a mostrar
         num_filas = st.slider("Número de filas a mostrar:", 1, len(data), 10)
+        st.write("En esta sección puedes consultar y etiquetar votaciones de la Cámara de Diputados de Chile. señalando la cantidad necesaria que quiera mostrar a continuación.")
         with st.form(key='multiselect_form'):
             columnas = list(data.columns)
             columnas_seleccionadas = st.multiselect("Selecciona las columnas a mostrar:", columnas, default=columnas[11:23])
@@ -33,13 +34,31 @@ def show_consulta(data,actualizar_dataframe):
             st.session_state.first_load = True
 
         if submit_button or st.session_state.first_load or num_filas:
+            st.write("aqui se mostrara el resultado de las columnas seleccionadas y la cantidad de filas que se le asigno. siendo las columnas las siguientes:")
+            st.write("votaciones_Nombre:estos son los nombres de las votaciones que se realizaron en la camara de diputados.")
+            st.write("Texto:Este es el conjunto total de las opiniones realizadas en la camara de diputados.")
+            etiquetas = [
+        "Seguridad y Defensa",
+        "Relaciones Internacionales",
+        "Energía y Medioambiente",
+        "Justicia y Derechos Humanos",
+        "Educación",
+        "Políticas Sociales",
+        "Deporte, Cultura y Salud",
+        "Política Económica",
+        "Política Interna",
+        "Participación Ciudadana"
+            ]
+            st.write("Etiquetas seleccionadas: Estas son las etiquetas que se le asignaron a las votaciones realizadas en la camara de diputados. estas pueden ser:")
+            st.markdown('\n'.join([f"{i+1}. {etiqueta}" for i, etiqueta in enumerate(etiquetas)]))
             st.write("Datos del dataframe:", data[columnas_seleccionadas].head(num_filas))
             st.session_state.first_load = False
     else:
         st.warning("No se pudo obtener el dataframe.")
-    
+
     with st.form(key='etiquetado_form'):
         texto_a_etiquetar = st.text_input("Ingresa el texto a etiquetar:")
+        st.write("Aqui podra ingresar un texto para ser etiquetado por nuestro modelo en donde se le mostrara el resultado de las etiquetas que se le asignaron. Siendo el 1 una aparicion de la etiqueta y el 0 una no aparicion de la etiqueta.")
         submit_button = st.form_submit_button(label='Enviar')
 
         if submit_button:
